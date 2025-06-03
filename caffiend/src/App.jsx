38de9c182339a506da4a6e05e5763967b1c21573
csdavids519 <csdavids@proton.me>
2025-06-03 15:18:31 +0200

    First commit with basic components and hero section

---
+++ caffiend/src/App.jsx
@@ -0,0 +1,28 @@
+import CoffeeForm from "./components/CoffeeForm"
+import Hero from "./components/Hero"
+import Layout from "./components/Layout"
+import Stats from "./components/Stats"
+import History from "./components/History"
+
+
+function App() {
+
+  const isAuthenticated = false
+
+  const authenticatedContent = (
+    <>
+      <Stats />
+      <History />
+    </>
+  )
+
+  return (
+    <Layout>
+      <Hero />
+      <CoffeeForm />
+      {isAuthenticated && (authenticatedContent)}  
+    </Layout>
+  )
+}
+
+export default App
