16ebcf5528a05a75b764525c68582ce43f22a41a
csdavids519 <csdavids@proton.me>
2025-06-04 15:13:38 +0200

    Build out History

--- caffiend/src/components/History.jsx
+++ caffiend/src/components/History.jsx
@@ -1,7 +1,34 @@
+import { calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getCaffeineAmount, timeSinceConsumption } from "../utils";
+
 export default function History() {
+
     return (
         <>
+            <div className="section-header">
+                <i className="fa-solid fa-timeline" />
+                <h2>History</h2>
+            </div>
+            <p><i>Hover for more information!</i></p>
+            <div className="coffee-history">
+                {/* sort the data by time */}
+                {Object.keys(coffeeConsumptionHistory).sort((a, b) => b - a).map((utcTime, coffeeIndex) => {
+                    const coffee = coffeeConsumptionHistory[utcTime]
+                    const timeSinceConsume = timeSinceConsumption(utcTime)
+                    const originalAmount = getCaffeineAmount(coffee.name)
+                    const remainingAmount = calculateCurrentCaffeineLevel({
+                        [utcTime]: coffee
+                    })
 
+                    // $ is template literals - adds the $ to the value
+                    const summary = `${coffee.name} | ${timeSinceConsume} | $${coffee.cost} | ${remainingAmount}mg / ${originalAmount}mg`
+
+                    return (
+                        <div title={summary} key={coffeeIndex}>
+                            <i className="fa-solid fa-mug-hot" />
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
